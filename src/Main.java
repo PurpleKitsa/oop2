7b693a222892a5f6bbf368993833690af5816c0d
PurpleKitsa <kapitandiman2@gmail.com>
2023-01-29 19:12:59 +0300

    Коллекции. Часть 2. Хеш и хеш-функции

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
 import transport.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-    /*      DriverB driverB = new DriverB("Алёшин Михаил Петрович", true,5);
+         DriverB driverB = new DriverB("Алёшин Михаил Петрович", true,5);
             DriverB driverB2 = new DriverB("",true,6);
             DriverB driverB3 = new DriverB("",true,6);
             DriverB driverB4 = new DriverB("",true,6);
-            Car lada = new Car("lada ", "Vesta", 1.6, driverB);
+    /*      Car lada = new Car("lada ", "Vesta", 1.6, driverB);
             Car fiat = new Car("fiat", "turbo", 2.6, driverB2);
             Car porshe = new Car("porshe", "sport", 3.0, driverB3);
             Car audi = new Car("audi", "nitro", 3.1, driverB3);
             System.out.println(lada);
             System.out.println(fiat);
             System.out.println(porshe);
-            System.out.println(audi);
+            System.out.println(audi); */
 
-            DriverD driverD = new DriverD("",true,6);
+            DriverD driverD = new DriverD("Алексеев Алеклей Алексеевич",true,6);
             DriverD driverD2 = new DriverD("",true,6);
             DriverD driverD3 = new DriverD("",true,6);
             DriverD driverD4 = new DriverD("",true,6);
-            Bus ladaB = new Bus("lada", "Speed", 1.8, driverD);
+    /*      Bus ladaB = new Bus("lada", "Speed", 1.8, driverD);
             Bus mazda = new Bus("mazda", "GTR", 2.0, driverD2);
             Bus nissan = new Bus("nissan", "Sport 2.0", 2.2, driverD3);
             Bus mitshubishi = new Bus("mitshubishi", "drift", 2.0, driverD4);
@@ -31,13 +33,13 @@ public class Main {
             System.out.println(ladaB);
             System.out.println(mazda);
             System.out.println(nissan);
-            System.out.println(mitshubishi);
+            System.out.println(mitshubishi);  */
 
-            DriverC driverC = new DriverC("",true,6);
-            DriverC driverC2 = new DriverC("",true,6);
+            DriverC driverC = new DriverC("Никитин Никита Никитьевич",true,6);
+            DriverC driverC2 = new DriverC("Никитин Алеклей Иванович",true,6);
             DriverC driverC3 = new DriverC("",true,6);
             DriverC driverC4 = new DriverC("",true,6);
-            Truck toyota = new Truck("toyota", "cube", 3.7, driverC,);
+    /*        Truck toyota = new Truck("toyota", "cube", 3.7, driverC,);
             Truck bmw = new Truck("bmw", "legacy", 4.0, driverC2);
             Truck jeep = new Truck("jeep", "ultra", 3.9, driverC3);
             Truck ferrari = new Truck("ferrari", "musctang", 4.2, driverC4);
@@ -50,7 +52,7 @@ public class Main {
         }
         private static void printInfo (Transport < ? > transport){
             System.out.println("Водитель " + transport.getDriver().getName() + " управляет автомобилем " + transport.getBrand() + " и будет участвовать в заезде");
-        }*/
+        }
         // System.out.println(Truck.LoadCapacity.N1);
         // System.out.println(Car.BodyType.SEDAN);
         // System.out.println(Bus.Capacity.BIG);
@@ -58,11 +60,11 @@ public class Main {
         Car bmw = new Car("bmw", "legacy", 4.0, driverB3, Car.BodyType.COUPE);
         DriverB driverB = new DriverB("Алёшин Михаил Петрович", true, 6);
         Car lada = new Car("lada ", "Vesta", 1.6, driverB, Car.BodyType.SEDAN);
-       /* try {
+       try {
             lada.passDiagnostics();
         } catch (DiagnosticFailedException e) {
             throw new RuntimeException(e);
-        }*/
+        }
         Mechanic mec1 = new Mechanic("Иванов Иван Иванович ", " Быстрый ремонт");
         Mechanic mec2 = new Mechanic("Петров Петр Петрович ", " Любой ремонт");
         Mechanic mec3 = new Mechanic("Васильев Василий Васильевич ", " Быстрый ремонт");
@@ -78,7 +80,19 @@ public class Main {
 
         for (Transport<?> transport : mechAndDriver) {
             System.out.println(transport + " " + transport.getDriver() + " " + transport.getMechanics());
-        }
+        } */
+
+        List<Driver> drivers = new ArrayList<>();
+        drivers.add(driverB);
+        drivers.add(driverB);
+        drivers.add(driverC);
+        drivers.add(driverC2);
+        drivers.add(driverD);
+
+
+        Set<Driver> driverSet = new HashSet<>(drivers);
+
+        System.out.println(driverSet);
     }
 }
 
